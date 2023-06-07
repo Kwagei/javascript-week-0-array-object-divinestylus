08371d16a12f8bd2839a2a131dbf84553dd8d6cd
divinestylus <divinestylus@gmail.com>
2023-06-07 00:26:50 +0000

    Problem 3

---
+++ problem3.js
@@ -0,0 +1,12 @@
+let iclass = {
+    name: 'Sam',
+    male: true,
+    phone: 0880442708   
+}
+
+iclass.address = 'Paynesville';
+
+console.log(iclass.name);
+console.log( iclass['name'] );
+
+console.table(iclass);
